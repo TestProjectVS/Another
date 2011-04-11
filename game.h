c5896719b80a12c757e5b505e61c78c081b5462f
unknown <Niklas@.(none)>
2011-04-11 23:18:52 +0200

    Signed-off-by: unknown <Niklas@.(none)>

---
+++ game.h
@@ -0,0 +1,30 @@
+#ifndef __GAME_H__
+#define __GAME_H__
+
+#include <string>
+#include <map>
+
+/*	there should only be one game instance
+	but as singletons are evil, let the user of it manage it
+*/
+
+
+// We don't want to show the whole world the lua_State type, so just forward declare it
+struct lua_State;
+
+class Game
+{
+public:
+	typedef std::map<std::string, std::string> tValues;
+
+	int RegisterLuaInterface(lua_State *l);
+	void SetProperty(std::string key, std::string value);
+	std::string GetProperty(std::string key);
+	void PrintValue(lua_State *l);
+private:
+	static int lua_getproperty(lua_State *L);
+	static int lua_setproperty(lua_State *L);
+	tValues mValues;
+};
+
+#endif
